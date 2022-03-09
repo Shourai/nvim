f74c181fe14075b35cf5755559d182845adefaf8
Shourai <10200748+Shourai@users.noreply.github.com>
2022-03-09 21:14:27 +0100

    Move packer_compiled.lua to data folder

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -5,7 +5,9 @@ if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
-return require('packer').startup(function(use)
+return require('packer').startup({
+  config = { compile_path  = vim.fn.stdpath('data') .. '/site/pack/loader/start/packer.nvim/plugin/packer_compiled.lua'},
+  function(use)
 
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -121,4 +123,4 @@ return require('packer').startup(function(use)
   if packer_bootstrap then
     require('packer').sync()
   end
-end)
+end})
