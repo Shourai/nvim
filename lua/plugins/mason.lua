6d2266e5dc619ce74a3aee5a8d5ceb9bedbd3a6e
Shourai <10200748+Shourai@users.noreply.github.com>
2022-11-09 21:52:27 +0100

    Add Mason

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,11 @@
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+require("mason-lspconfig").setup({
+    ensure_installed = { "sumneko_lua", "ansiblels", "bashls", "dockerls",
+    "gopls", "html", "tsserver", "marksman", "pyright" }
+})
+
+require("mason-null-ls").setup({
+    ensure_installed = { "stylua", "jq", "prettier", "eslint_d", "markdownlint", "flake8", "shellcheck", "yamllint" }
+})
