fe3ddbf5794fd9a38eb599198e8e9dd24818d7ba
Shourai <10200748+Shourai@users.noreply.github.com>
2022-11-09 21:52:42 +0100

    Set recommended option at nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,3 +1,6 @@
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
