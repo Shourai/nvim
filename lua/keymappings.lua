dbd04a73735f215e535263558116076a74819974
Shourai <10200748+Shourai@users.noreply.github.com>
2022-11-15 18:27:03 +0100

    Remove buffer navigation keymaps

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -7,10 +7,6 @@ map("n", "<C-j>", "<C-w>j", default_opts)
 map("n", "<C-k>", "<C-w>k", default_opts)
 map("n", "<C-l>", "<C-w>l", default_opts)
 
--- Navigate buffers
-map("n", "<S-l>", ":bnext<CR>", default_opts)
-map("n", "<S-h>", ":bprevious<CR>", default_opts)
-
 -- telescope
 map("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", default_opts)
 map("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", default_opts)
@@ -18,9 +14,9 @@ map("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", de
 map("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", default_opts)
 
 -- nvim-tree
-map('n', '<C-n>', ':NvimTreeToggle<CR>', default_opts)       -- open/close
-map('n', '<leader>r', ':NvimTreeRefresh<CR>', default_opts)  -- refresh
-map('n', '<leader>n', ':NvimTreeFindFile<CR>', default_opts) -- search file
+map("n", "<C-n>", ":NvimTreeToggle<CR>", default_opts) -- open/close
+map("n", "<leader>r", ":NvimTreeRefresh<CR>", default_opts) -- refresh
+map("n", "<leader>n", ":NvimTreeFindFile<CR>", default_opts) -- search file
 
 -- Toggleterm
 map("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", default_opts)
