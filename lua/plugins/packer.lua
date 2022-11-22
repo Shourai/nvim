51b101332cef86202d5447660b5047a76745d20e
Shourai <10200748+Shourai@users.noreply.github.com>
2022-11-22 20:32:24 +0100

    Use nvim-surround instead of vim-surround

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -87,7 +87,15 @@ return require("packer").startup({
 		use({ "lukas-reineke/indent-blankline.nvim" })
 
 		-- Vim surround
-		use("tpope/vim-surround")
+		use({
+			"kylechui/nvim-surround",
+			tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+			config = function()
+				require("nvim-surround").setup({
+					-- Configuration here, or leave empty to use defaults
+				})
+			end,
+		})
 
 		-- Git signs
 		use({
