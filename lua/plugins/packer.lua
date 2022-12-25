1420b4d90061c6da08f12689d09139c4243207ba
Shourai <10200748+Shourai@users.noreply.github.com>
2022-12-25 12:58:54 +0100

    Update nvim-tree

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -77,8 +77,10 @@ return require("packer").startup({
 
 		-- nvim-tree
 		use({
-			"kyazdani42/nvim-tree.lua",
-			requires = "kyazdani42/nvim-web-devicons",
+			"nvim-tree/nvim-tree.lua",
+			requires = {
+				"nvim-tree/nvim-web-devicons", -- optional, for file icons
+			},
 		})
 
 		-- Toggleterm
