71162972e8b59c5086e6b665bf7153e52aa0e112
Shourai <10200748+Shourai@users.noreply.github.com>
2022-12-31 17:17:00 +0100

    Cleanup code

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -151,11 +151,7 @@ return require("packer").startup({
 		use({
 			"folke/which-key.nvim",
 			config = function()
-				require("which-key").setup({
-					-- your configuration comes here
-					-- or leave it empty to use the default settings
-					-- refer to the configuration section below
-				})
+				require("which-key").setup()
 			end,
 		})
 
