ddcc3a47814b046e63f093b8e29197aff160c1e9
Shourai <10200748+Shourai@users.noreply.github.com>
2023-01-10 15:51:35 +0100

    Set flake8 and autopep8 to linelength of 120

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -4,9 +4,9 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.jq,
-		null_ls.builtins.formatting.autopep8,
+		null_ls.builtins.formatting.autopep8.with({ extra_args = { "--max-line-length", "120" } }),
 		null_ls.builtins.diagnostics.eslint_d,
-		null_ls.builtins.diagnostics.flake8,
+		null_ls.builtins.diagnostics.flake8.with({ extra_args = { "--max-line-length", "120" } }),
 		null_ls.builtins.diagnostics.yamllint,
 	},
 })
