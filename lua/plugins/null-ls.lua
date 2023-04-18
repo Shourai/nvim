79c89298fc8b1a53b9c34946e40691a7db8dca6f
Shourai <10200748+Shourai@users.noreply.github.com>
2023-04-18 11:13:39 +0200

    Add config file location to yamlfmt

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -4,7 +4,7 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.jq,
-		null_ls.builtins.formatting.yamlfmt,
+		null_ls.builtins.formatting.yamlfmt.with({ extra_args = { "--conf", vim.fn.expand("~/.config/ymlfmt/.yamlfmt") } }),
 		null_ls.builtins.formatting.autopep8.with({ extra_args = { "--max-line-length", "120" } }),
 		null_ls.builtins.diagnostics.eslint_d,
 		null_ls.builtins.diagnostics.flake8.with({ extra_args = { "--max-line-length", "120" } }),
