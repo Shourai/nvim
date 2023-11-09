597efd718c672c388b466285fd6c19f538c725d2
Shourai <10200748+Shourai@users.noreply.github.com>
2023-11-09 14:56:56 +0100

    Update mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,15 +1,51 @@
 return {
-  {"williamboman/mason.nvim",
-    config  = function()
-      require("mason").setup()
-    end
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
-  {"williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ansiblels", "bashls", "dockerls",
-          "gopls", "html", "tsserver", "marksman", "pyright" }
-      })
-    end
-  },
+  config = function()
+    -- import mason
+    local mason = require("mason")
+
+    -- import mason-lspconfig
+    local mason_lspconfig = require("mason-lspconfig")
+
+    local mason_tool_installer = require("mason-tool-installer")
+
+    -- enable mason and configure icons
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+
+    mason_lspconfig.setup({
+      -- list of servers for mason to install
+      ensure_installed = {
+        "tsserver",
+        "html",
+        "lua_ls",
+        "pyright",
+        "ruff-lsp",
+      },
+      -- auto-install configured servers (with lspconfig)
+      automatic_installation = true, -- not the same as ensure_installed
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "ruff", -- python linter
+        "eslint_d", -- js linter
+      },
+    })
+  end,
 }
