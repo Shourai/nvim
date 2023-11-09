6434f1dcd3ca752c882961ebfca9459cc3c2b825
Shourai <10200748+Shourai@users.noreply.github.com>
2023-11-09 15:13:23 +0100

    Update nvim-lint

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,30 +1,26 @@
 return {
-	"mfussenegger/nvim-lint",
-	lazy = true,
-	event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
-	config = function()
-		local lint = require("lint")
+  "mfussenegger/nvim-lint",
+  lazy = true,
+  config = function()
+    local lint = require("lint")
 
-		lint.linters_by_ft = {
-			javascript = { "eslint_d" },
-			typescript = { "eslint_d" },
-			javascriptreact = { "eslint_d" },
-			typescriptreact = { "eslint_d" },
-			svelte = { "eslint_d" },
-			python = { "ruff" },
-		}
+    lint.linters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      python = { "ruff" },
+    }
 
-		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+    vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
+      callback = function()
+        local lint_status, lint = pcall(require, "lint")
+        if lint_status then
+          lint.try_lint()
+        end
+      end,
+    })
 
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-			group = lint_augroup,
-			callback = function()
-				lint.try_lint()
-			end,
-		})
-
-		vim.keymap.set("n", "<leader>l", function()
-			lint.try_lint()
-		end, { desc = "Trigger linting for current file" })
-	end,
+    vim.keymap.set("n", "<leader>l", function()
+      lint.try_lint()
+    end, { desc = "Trigger linting for current file" })
+  end,
 }
