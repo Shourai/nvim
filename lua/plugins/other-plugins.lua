f7640103fac0f6f8a76d754045df3a00bbe73548
Shourai <10200748+Shourai@users.noreply.github.com>
2024-11-15 09:40:55 +0100

    Move nvim-surround into its own file

--- lua/plugins/other-plugins.lua
+++ lua/plugins/other-plugins.lua
@@ -2,17 +2,6 @@ return {
 	-- Easy align
 	"junegunn/vim-easy-align",
 
-	-- Vim surround
-	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end,
-	},
-
 	-- Git signs
 	{
 		"lewis6991/gitsigns.nvim",
