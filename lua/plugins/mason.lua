23ece8a92debaf248f569f7cb9fa812854308c24
Shourai <10200748+Shourai@users.noreply.github.com>
2024-11-15 09:41:12 +0100

    Update Mason: ruff_lsp -> ruff and tsserver -> typescript-language-server

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -27,11 +27,11 @@ return {
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
-				"tsserver",
+				"typescript-language-server",
 				"html",
 				"lua_ls",
 				"pyright",
-				"ruff_lsp",
+				"ruff",
 			},
 			-- auto-install configured servers (with lspconfig)
 			automatic_installation = true, -- not the same as ensure_installed
