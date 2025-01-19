607a52252b17683103272d982f9d3f90bd774e6d
Shourai <10200748+Shourai@users.noreply.github.com>
2025-01-19 14:01:04 +0100

    Set OSC52 clipboard only when $SSH_TTY is detected, otherwise use local clipboard tools

--- lua/settings.lua
+++ lua/settings.lua
@@ -28,17 +28,19 @@ local function paste()
 	}
 end
 
-vim.g.clipboard = {
-	name = "OSC 52",
-	copy = {
-		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-	},
-	paste = {
-		["+"] = paste,
-		["*"] = paste,
-	},
-}
+if os.getenv("SSH_TTY") ~= nil then
+	vim.g.clipboard = {
+		name = "OSC 52",
+		copy = {
+			["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+			["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+		},
+		paste = {
+			["+"] = paste,
+			["*"] = paste,
+		},
+	}
+end
 -----------------------------------------------------------
 -- User Interface
 -----------------------------------------------------------
