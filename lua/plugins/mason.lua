4268f9ece3257833644bed5626d0352328aa70ee
Shourai <10200748+Shourai@users.noreply.github.com>
2025-01-20 09:56:45 +0100

    Revert "rename ts_ls to tsserver"
    This reverts commit 65874f112687cf7b65870bf38d682be959d48178.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -27,8 +27,8 @@ return {
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
+				"ts_ls",
 				"html",
-				"tsserver",
 				"lua_ls",
 				"pyright",
 				"ruff_lsp",
