59ced7a50182a6d30d903d72ff990e0c83aa55f0
Shourai <10200748+Shourai@users.noreply.github.com>
2025-04-24 15:59:20 +0200

    Use built-in neovim lsp config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,17 +1,8 @@
 return {
 	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
 	config = function()
 		-- import mason
 		local mason = require("mason")
-
-		-- import mason-lspconfig
-		local mason_lspconfig = require("mason-lspconfig")
-
-		local mason_tool_installer = require("mason-tool-installer")
 
 		-- enable mason and configure icons
 		mason.setup({
@@ -21,31 +12,6 @@ return {
 					package_pending = "➜",
 					package_uninstalled = "✗",
 				},
-			},
-		})
-
-		mason_lspconfig.setup({
-			-- list of servers for mason to install
-			ensure_installed = {
-				"ts_ls",
-				"html",
-				"lua_ls",
-				"pyright",
-				"ruff",
-			},
-			-- auto-install configured servers (with lspconfig)
-			automatic_installation = true, -- not the same as ensure_installed
-		})
-
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"prettier", -- prettier formatter
-				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
-				"ruff", -- python linter
-				"eslint_d", -- js linter
-				"markdownlint", -- markdown linter
 			},
 		})
 	end,
