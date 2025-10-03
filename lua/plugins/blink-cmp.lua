2a11649d827358d3b1ec24a214638eb0ecdc7319
Shourai <10200748+Shourai@users.noreply.github.com>
2025-10-03 23:58:11 +0200

    Disable signature for built-in one -> `CTRL-S`

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -26,7 +26,7 @@ return {
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = { preset = 'enter' },
-    signature = { enabled = true },
+    signature = { enabled = false },
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
