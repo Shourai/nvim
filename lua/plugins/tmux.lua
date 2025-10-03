12649d80f8e780dd85807da4c08ad31e2dfd9857
Shourai <10200748+Shourai@users.noreply.github.com>
2025-10-03 22:00:01 +0200

    Don't redirect system clipboard to tmux clipboard

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,1 +1,11 @@
-return { "aserowy/tmux.nvim", opts = {} }
+return {
+  "aserowy/tmux.nvim",
+  opts = {
+    copy_sync = {
+      -- overwrites vim.g.clipboard to redirect * and + to the system
+      -- clipboard using tmux. If you sync your system clipboard without tmux,
+      -- disable this option!
+      sync_clipboard = false,
+    },
+  }
+}
