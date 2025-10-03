3e26c9cc37d6f887f9655c81c1701f709118b9b6
Shourai <10200748+Shourai@users.noreply.github.com>
2025-10-03 22:00:13 +0200

    Add format on save autocommand

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -4,3 +4,23 @@ vim.lsp.enable({
   "lua_ls",
 })
 vim.diagnostic.config({ virtual_lines = {current_line = true}, virtual_text = false })
+
+-- autocmd to format on save
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('my.lsp', {}),
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    -- Auto-format ("lint") on save.
+    -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+    if not client:supports_method('textDocument/willSaveWaitUntil')
+        and client:supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = vim.api.nvim_create_augroup('my.lsp', {clear=false}),
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+        end,
+      })
+    end
+  end,
+})
