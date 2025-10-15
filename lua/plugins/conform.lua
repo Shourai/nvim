4b280cb2c4740236ee25ce14226c37dabe6b38a4
Shourai <10200748+Shourai@users.noreply.github.com>
2025-10-15 12:57:08 +0200

    Use prettier as formatter for multiple files

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,8 +2,14 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      markdown = { "markdownlint-cli2" },
+      markdown = { "prettier" },
       sh = { "shellcheck", "shfmt" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
     },
     format_after_save = {
       lsp_format = "fallback",
