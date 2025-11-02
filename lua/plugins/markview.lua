d0c48911c9ebc3bcbe43ec3e25b8920e9963460c
Shourai <10200748+Shourai@users.noreply.github.com>
2025-11-02 20:50:01 +0100

    Add keybinding to toggle splitview

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,7 +1,9 @@
 return {
   "OXY2DEV/markview.nvim",
   opts = {
-    preview = { enable = false }
+    preview = { enable = false },
+    vim.api.nvim_set_keymap("n", "<leader>ms", "<CMD>Markview splitToggle<CR>",
+      { desc = "Toggles `markview` splitview." }),
   },
 
   -- Completion for `blink.cmp`
