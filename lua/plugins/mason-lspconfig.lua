3245e9b021dffeae02081f21f77d91c1775f2ec8
Shourai <10200748+Shourai@users.noreply.github.com>
2025-11-19 21:11:49 +0100

    Use `MasonInstallAll` instead of ensure_installed

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,14 +1,24 @@
 return {
   "mason-org/mason-lspconfig.nvim",
   opts = {
-    ensure_installed = {
-      -- lsp
-      "lua_ls",
-      "pyright",
-      "ruff",
-      "ts_ls",
-      "yamlls",
-    },
+    -- mason, write correct names only
+    vim.api.nvim_create_user_command("MasonInstallAll", function()
+      local packages = {
+        "jsonlint",
+        "lua-language-server",
+        "markdownlint-cli2",
+        "marksman",
+        "prettier",
+        "pyright",
+        "ruff",
+        "shellcheck",
+        "shfmt",
+        "typescript-language-server",
+        "yaml-language-server",
+        "yamllint",
+      }
+      vim.cmd("MasonInstall " .. table.concat(packages, " "))
+    end, {})
   },
   dependencies = {
     { "mason-org/mason.nvim", opts = {} },
