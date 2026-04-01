7429cae017cdc791406189a609ef96dd5cc014d6
Shourai <10200748+Shourai@users.noreply.github.com>
2026-04-01 15:20:14 +0200

    Update treesitter-textobjects for nvim >= 0.12

--- lua/plugins/treesitter-textobjects.lua
+++ lua/plugins/treesitter-textobjects.lua
@@ -1,68 +1,61 @@
 return {
   "nvim-treesitter/nvim-treesitter-textobjects",
-  main = "nvim-treesitter.configs",
-  opts = {
-    textobjects = {
+  branch = "main", -- Ensure you are tracking the main branch
+  init = function()
+    -- Disable built-in ftplugin mappings to prevent conflicts with your custom ones
+    -- This is recommended by the plugin documentation
+    vim.g.no_plugin_maps = true
+  end,
+  config = function()
+    local ts_select = require("nvim-treesitter-textobjects.select")
+
+    require("nvim-treesitter-textobjects").setup({
       select = {
         enable = true,
-
-        -- Automatically jump forward to textobj, similar to targets.vim
         lookahead = true,
-
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
-          ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
-          ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
-          ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
-
-          ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
-          ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
-
-          ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
-          ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
-
-          ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
-          ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
-
-          ["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
-          ["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
-
-          ["am"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
-          ["im"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
-
-          ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
-          ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
-
-          ["ib"] = { query = "@block.inner", desc = "Select inner part of a markdown code fence" },
-          ["ab"] = { query = "@block.outer", desc = "Select outer part of a markdown code fence" },
-
-          ["i`"] = { query = "@block.inner", desc = "Select inner part of a markdown code fence" },
-          ["a`"] = { query = "@block.outer", desc = "Select outer part of a markdown code fence" },
-        },
-        -- You can choose the select mode (default is charwise 'v')
-        --
-        -- Can also be a function which gets passed a table with the keys
-        -- * query_string: eg '@function.inner'
-        -- * method: eg 'v' or 'o'
-        -- and should return the mode ('v', 'V', or '<c-v>') or a table
-        -- mapping query_strings to modes.
-        selection_modes = {
-          ['@parameter.outer'] = 'v', -- charwise
-          ['@function.outer'] = 'V',  -- linewise
-          ['@class.outer'] = 'v',     -- blockwise
-        },
-        -- If you set this to `true` (default is `false`) then any textobject is
-        -- extended to include preceding or succeeding whitespace. Succeeding
-        -- whitespace has priority in order to act similarly to eg the built-in
-        -- `ap`.
-        --
-        -- Can also be a function which gets passed a table with the keys
-        -- * query_string: eg '@function.inner'
-        -- * selection_mode: eg 'v'
-        -- and should return true or false
         include_surrounding_whitespace = true,
+        selection_modes = {
+          ["@parameter.outer"] = "v",
+          ["@function.outer"] = "V",
+          ["@class.outer"] = "v",
+        },
       },
-    },
-  }
+    })
+
+    -- Define Keymaps manually for the 'main' branch
+    local mappings = {
+      -- Assignments
+      ["a="] = "@assignment.outer",
+      ["i="] = "@assignment.inner",
+      ["l="] = "@assignment.lhs",
+      ["r="] = "@assignment.rhs",
+      -- Parameters/Arguments
+      ["aa"] = "@parameter.outer",
+      ["ia"] = "@parameter.inner",
+      -- Conditionals/Loops
+      ["ai"] = "@conditional.outer",
+      ["ii"] = "@conditional.inner",
+      ["al"] = "@loop.outer",
+      ["il"] = "@loop.inner",
+      -- Functions/Calls
+      ["af"] = "@call.outer",
+      ["if"] = "@call.inner",
+      ["am"] = "@function.outer",
+      ["im"] = "@function.inner",
+      -- Classes
+      ["ac"] = "@class.outer",
+      ["ic"] = "@class.inner",
+      -- Blocks/Markdown
+      ["ib"] = "@block.inner",
+      ["ab"] = "@block.outer",
+      ["i`"] = "@block.inner",
+      ["a`"] = "@block.outer",
+    }
+
+    for key, query in pairs(mappings) do
+      vim.keymap.set({ "x", "o" }, key, function()
+        ts_select.select_textobject(query, "textobjects")
+      end, { desc = "Select " .. query })
+    end
+  end,
 }
