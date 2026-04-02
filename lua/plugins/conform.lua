bd7df99470784497f02f7ef7573cd87117a425bc
Shourai <10200748+Shourai@users.noreply.github.com>
2026-04-02 16:19:13 +0200

    Use ansible-lint and shfmt for conform.nvim

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,13 +2,13 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      ansible = { "ansible-lint --fix" },
+      ansible = { "ansible-lint" },
       css = { "prettier" },
       html = { "prettier" },
       javascript = { "prettier" },
       json = { "prettier" },
       markdown = { "prettier" },
-      sh = { "shellcheck", "shfmt" },
+      sh = { "shfmt" },
       terraform = { "tofu_fmt" },
       typescript = { "prettier" },
       yaml = { "prettier" },
